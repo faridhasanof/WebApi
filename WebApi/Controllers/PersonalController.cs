37cdcd924a02d282228fd5822c77f580cd9e7817
faridhasanov475 <feridhesenov8847@gmail.com>
2022-04-09 19:11:29 +0400

    Add project files.

---
+++ WebApi/Controllers/PersonalController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class PersonalController : ApiController
+    {
+        public List<Personal> GetPersonals()
+        {
+            return PersonalList.pers;
+        }
+        public Personal GetPersonal(int id)
+        {
+            Personal ischi = PersonalList.pers.Find(o => o.id == id);
+            return ischi;
+        }
+    }
+}
---
+++ WebApi/PersonalList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi
+{
+    public class PersonalList
+    {
+        public static List<Personal> pers = new List<Personal>
+        {
+            new Personal
+            {
+                id=1000,
+                name="Ibo",
+                surname="Seyfo",
+                birth_place="Salyan",
+                age=20,
+                salary=1500
+            },
+             new Personal
+            {
+                id=1001,
+                name="Kenan",
+                surname="Hesenov",
+                birth_place="Goycay",
+                age=20,
+                salary=1800
+            },
+              new Personal
+            {
+                id=1002,
+                name="Banu",
+                surname="Xuduyeva",
+                birth_place="Fuzuli",
+                age=20,
+                salary=2000
+            }
+        };-
+    }
+}
